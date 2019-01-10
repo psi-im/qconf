ebecd35187d5207bd48abea00cb27f8fc7fb55b0
Boris Pek <tehnick-8@yandex.ru>
2019-01-11 00:30:55 +0300

    Fix cppcheck notes with severity "style":
    Array index 'i' is used before limits check.

--- conf/conf4.cpp
+++ conf/conf4.cpp
@@ -215,8 +215,9 @@ QStringList qc_splitflags(const QString &flags)
 					continue;
 				}
 			} else { // we are in double quoetes
-				if (flags[i] == backslash && i < flags.length() - 1 &&
-						(flags[i+1] == QLatin1Char('"') || flags[i+1] == backslash))
+				if (flags[i] == backslash &&
+					(flags[i+1] == QLatin1Char('"') || flags[i+1] == backslash) &&
+					i < flags.length() - 1)
 				{
 					// if next symbol is one of in parentheses ("\)
 					escaped = true;
