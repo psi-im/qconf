61ccb744f97e549bdda6556bb7f86cf73768da0e
Boris Pek <tehnick-8@yandex.ru>
2020-11-23 20:36:06 +0300

    Fix cppcheck note:
    Array index 'i' is used before limits check.

--- conf/conf4.cpp
+++ conf/conf4.cpp
@@ -206,7 +206,7 @@ QStringList qc_splitflags(const QString &flags)
                     continue;
                 }
             } else { // we are in double quoetes
-                if (flags[i] == backslash && i < flags.length() - 1
+                if (i < flags.length() - 1 && flags[i] == backslash
                     && (flags[i + 1] == QLatin1Char('"') || flags[i + 1] == backslash)) {
                     // if next symbol is one of in parentheses ("\)
                     escaped = true;
